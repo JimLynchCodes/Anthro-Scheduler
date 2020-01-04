e9d998ca44a893b1da0d19451525a42531b6def2
JimTheMan <mrdotjim@gmail.com>
2020-01-04 15:51:54 -0500

    initial commit

---
+++ add-random-time-within-bounds.js
@@ -0,0 +1,17 @@
+
+var constants = require('./constants')
+
+const addRandomTimeWithinBounds = (initialTime) => {
+
+    const secondsInterval = constants.INTERVAL_MAX_SECONDS - constants.INTERVAL_MIN_SECONDS;
+    const secondstoAdd = constants.INTERVAL_MIN_SECONDS + Math.floor(Math.random() * secondsInterval)
+
+    const minutesInterval = constants.INTERVAL_MAX_MINUTES - constants.INTERVAL_MIN_MINUTES;
+    const minutestoAdd = constants.INTERVAL_MIN_MINUTES + Math.floor(Math.random() * minutesInterval)
+
+    console.log(`adding ${minutestoAdd} minutes and ${secondstoAdd} seconds!`)
+
+    return initialTime.add(secondstoAdd, 'seconds').add(minutestoAdd, 'minutes')
+}
+
+module.exports = addRandomTimeWithinBounds;
---
+++ constants.js
@@ -0,0 +1,14 @@
+const constants = {
+    INTERVAL_MIN_SECONDS: 0,
+    INTERVAL_MAX_SECONDS: 55,
+
+    INTERVAL_MAX_MINUTES: 2,
+    INTERVAL_MIN_MINUTES: 0,
+
+    EARLIEST_TWEET_CUTOFF_HOUR: 8,
+    LATEST_TWEET_CUTOFF_HOUR: 21,
+    
+    COMMAND_TO_EXEUTE: 'cd /Users/jim/Git-Projects/Tweet-Spewer && npm test',
+}
+
+module.exports = constants
